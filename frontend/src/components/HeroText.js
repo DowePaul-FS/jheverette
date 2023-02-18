f3c92bee6ad37828200182bca9157be51a191d17
pdowejr <64440746+pdowejr@users.noreply.github.com>
2023-02-18 15:15:19 -0500

    Hero text rough draft

---
+++ frontend/src/components/HeroText.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import classes from './HeroText.module.css';
+
+const HeroText = (props) => {
+    return (
+        <div className={`
+        ${["container"]} 
+        ${classes["hero__text"]}
+        `}
+        >
+            <article className={`
+            ${["row"]} 
+            ${["row-cols-1"]}
+            `}
+            >
+                <h2 className={`
+                ${["col"]} 
+                ${classes["hero__title"]}
+                `}
+                >
+                    {props.title}
+                </h2>
+                <p className={`
+                ${["col"]} 
+                ${classes["hero__para"]}
+                `}
+                >
+                    {props.text}
+                </p>
+            </article>
+        </div>
+    );
+};
+
+export default HeroText;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import classes from './Home.module.css';
 import NavBar from "../components/NavBar";
+import HeroText from "../components/HeroText";
 
 const Home = () => {
     return (
         <main>
             <section className={classes["bg__img"]}>
                 <NavBar />
+                <HeroText 
+                title="Quality Web Design & Development for all your Business needs." 
+                text="We provide full-service website design, development, and search
+                optimization services for your business. We partner with your
+                company to provide professional web services and consultations"
+                />
             </section>
         </main>
     );
