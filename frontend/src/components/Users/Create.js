6dfe5938b17228da7e9faa968536d4eb5c3c273f
pdowejr <64440746+pdowejr@users.noreply.github.com>
2023-02-21 23:46:37 -0500

    Form classes

--- frontend/src/components/Users/Create.js
+++ frontend/src/components/Users/Create.js
@@ -5,26 +5,26 @@ import classes from './Create.module.css';
 const Create = (props) => {
     return (
         <Modal onClose={props.onClose}>
-            <form class="rd-mailform rd-mailform_responsive" data-form-output="form-output-global" data-form-type="contact" method="post" action="bat/rd-mailform.php">
-                <div class="form-wrap form-wrap_icon linear-icon-envelope">
-                    <input class="form-input" id="modal-register-email" type="email" name="email" data-constraints="@Email @Required" />
-                    <label class="form-label" for="modal-register-email">Your e-mail</label>
+            <form className={`${classes["rd-mailform"]} ${classes["rd-mailform_responsive"]}`} data-form-output="form-output-global" data-form-type="contact" method="post" action="bat/rd-mailform.php">
+                <div className={`${classes["form-wrap"]} ${classes["form-wrap_icon"]} ${classes["linear-icon-envelope"]}`}>
+                    <input className={`${classes["form-input"]}`} id={["modal-register-email"]} type="email" name="email" data-constraints="@Email @Required" />
+                    <label className={classes["form-label"]} for={["modal-register-email"]}>Your e-mail</label>
                 </div>
-                <div class="form-wrap form-wrap_icon linear-icon-lock">
-                    <input class="form-input" id="modal-register-password" type="password" name="password" data-constraints="@Required" />
-                    <label class="form-label" for="modal-register-password">Your password</label>
+                <div className={`${classes["form-wrap"]} ${classes["form-wrap_icon"]} ${classes["linear-icon-lock"]}`}>
+                    <input className={classes["form-input"]} id={["modal-register-password"]} type="password" name="password" data-constraints="@Required" />
+                    <label className={classes["form-label"]} for={["modal-register-password"]}>Your password</label>
                 </div>
-                <div class="form-wrap form-wrap_icon linear-icon-lock">
-                    <input class="form-input" id="modal-register-password2" type="password" name="password2" data-constraints="@Required" />
-                    <label class="form-label" for="modal-register-password2">Confirm password</label>
+                <div className={`${classes["form-wrap"]} ${classes["form-wrap_icon"]} ${classes["linear-icon-lock"]}`}>
+                    <input className={classes["form-input"]} id={["modal-register-password2"]} type="password" name="password2" data-constraints="@Required" />
+                    <label className={classes["form-label"]} for={["modal-register-password2"]}>Confirm password</label>
                 </div>
-                <div class="form-wrap">
-                    <label class="checkbox-inline">
+                <div className={classes["form-wrap"]}>
+                    <label className={classes["checkbox-inline"]}>
                         <input type="checkbox" name="remember" />Remember me
                     </label>
                 </div>
-                <button class="button button-primary" type="submit" onClick={props.onClose}>Close</button>
-                <button class="button button-primary" type="submit">Register</button>
+                <button className={`${classes["button"]} ${classes["button-primary"]}`} type="close" onClick={props.onClose}>Close</button>
+                <button className={`${classes["button"]} ${classes["button-primary"]}`} type="submit">Register</button>
             </form>
         </Modal>
     );
